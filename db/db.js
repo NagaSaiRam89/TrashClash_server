0a94e3d46a1f0880f00bd10e98b7e4d8b1e65756
SUNKARA NAGA SAI RAM <snsram089.mca2023@ca.nitrr.ac.in>
2025-07-07 00:36:52 +0530

    Update db.js
    db Integration

--- db/db.js
+++ db/db.js
@@ -1,3 +1,4 @@
+// db.js
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -6,6 +7,9 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required by Render for SSL connections
+  },
 });
 
 export default pool;
